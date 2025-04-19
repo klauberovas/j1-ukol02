cc596cb1aefa9d5eedbcb1ff74757c2e1d466ab1
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-04-19 18:02:45 +0200

    implement method for drawing train

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -17,12 +17,15 @@ public class HlavniProgram {
 
         //TODO Tady bude kód pro kreslení želví grafiky.
 //        drawIceCream(70.0);
-        drawSnowman();
+//        drawSnowman();
+        drawTrain();
     }
 
     // Part 2
     // Ice cream
     public void drawIceCream(double iceCreamDiameter) {
+        moveToDefaultPosition();
+
         drawIsocelesTriangle(iceCreamDiameter);
 
         int steps = 36;
@@ -33,7 +36,10 @@ public class HlavniProgram {
         drawPolygon(steps, stepLength, circleAngle);
     }
 
+    // Snowman
     public void drawSnowman() {
+        moveToDefaultPosition();
+
         int headSteps = 36;
         int middleSteps = 60;
         int bottomSteps = 90;
@@ -43,7 +49,6 @@ public class HlavniProgram {
 
         // Head
         double headRadius = (headSteps * stepLength) / (2 * Math.PI);
-        zofka.setLocation(400, 150);
         drawPolygon(headSteps, stepLength, 360.0 / headSteps);
 
         // Middle circle
@@ -64,6 +69,65 @@ public class HlavniProgram {
         // Left hand
         zofka.setLocation(zofka.getX() - handDiameter - (middleRadius * 2), zofka.getY());
         drawPolygon(handSteps, stepLength, 360.0 / handSteps); // left hand
+    }
+
+    public void drawTrain() {
+        int backWheelSteps = 60;
+        double stepLength = 6;
+        double backWheelDiameter = (backWheelSteps * stepLength) / Math.PI;
+        double cabinHeight = backWheelDiameter * 1.3;
+        double cabinWidth = backWheelDiameter;
+        double engineHeight = cabinHeight / 2;
+        double engineWidth = cabinHeight;
+        int frontWheelSteps = backWheelSteps / 2;
+        double frontWheelDiameter = backWheelDiameter / 2;
+        double frontWheelOffset = engineWidth * 0.1;
+
+        drawBackWheel(backWheelSteps, stepLength);
+        drawCabine(cabinHeight, cabinWidth);
+
+        zofka.setLocation(zofka.getX() - backWheelDiameter, zofka.getY());
+        zofka.turnLeft(90);
+        drawEngine(engineHeight, engineWidth);
+
+        // X-axis for plow
+        double frontTrainX = zofka.getX() - engineWidth;
+
+        // Front wheels
+        zofka.turnLeft(90);
+        drawFrontWheels(frontWheelSteps, stepLength, frontWheelDiameter, frontWheelOffset);
+
+        // Plow
+        zofka.setLocation(frontTrainX, zofka.getY());
+        drawPlow(engineHeight);
+    }
+
+    private void drawBackWheel(int segments, double segmentsLength) {
+        drawPolygon(segments, segmentsLength, 360.0 / segments);
+    }
+
+    private void drawCabine(double height, double width) {
+        drawRectangle(height, width);
+    }
+
+    private void drawEngine(double height, double width) {
+        drawRectangle(height, width);
+    }
+
+    private void drawFrontWheels(int steps, double stepLength, double diameter, double offset) {
+        zofka.setLocation(zofka.getX() - offset, zofka.getY() + diameter / 2);
+        drawPolygon(steps, stepLength, 360.0 / steps);
+
+        zofka.setLocation(zofka.getX() - diameter - 10, zofka.getY());
+        drawPolygon(steps, stepLength, 360.0 / steps);
+    }
+
+    private void drawPlow(double size) {
+        drawRightIsocelesTriangle(size);
+    }
+
+    public void moveToDefaultPosition() {
+        zofka.setLocation(zofka.getX(), 150);
     }
 
     // Part 1 - basic parametric shapes
@@ -87,13 +151,13 @@ public class HlavniProgram {
 
     private void drawRightIsocelesTriangle(double size) {
         zofka.move(size);
-        zofka.turnLeft(90.0);
-
-        zofka.move(size);
         zofka.turnLeft(135.0);
 
         zofka.move(Math.sqrt(2 * Math.pow(size, 2)));
         zofka.turnLeft(135.0);
+
+        zofka.move(size);
+        zofka.turnLeft(90.0);
     }
 
     private void drawSquare(double a) {
