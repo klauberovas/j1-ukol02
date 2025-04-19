e4111fd430f7f810d3483272bf1079a4d7770aeb
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-04-19 14:52:31 +0200

    implement method for drawing ice cream

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -16,20 +16,53 @@ public class HlavniProgram {
         zofka.setPenWidth(4);
 
         //TODO Tady bude kód pro kreslení želví grafiky.
-//        drawEquilateralTriangle(100.0);
-//        drawSquare(100.0);
-//        drawRectangle(50.0, 100.0);
-        drawPolygon(23, 12, 16);
+        drawIceCream(70.0);
     }
 
-    public void drawEquilateralTriangle(double size) {
+    // Part 2
+    // Ice cream
+    public void drawIceCream(double iceCreamDiameter) {
+        drawIsocelesTriangle(iceCreamDiameter);
+
+        int steps = 36;
+        double circleCircumference = Math.PI * iceCreamDiameter;
+        double stepLength = circleCircumference / steps;
+        double circleAngle = 360.0 / steps;
+
+        drawPolygon(steps, stepLength, circleAngle);
+    }
+
+    // Part 1 - basic parametric shapes
+    private void drawEquilateralTriangle(double size) {
         for (int i = 0; i < 3; i++) {
-            zofka.move(size);
             zofka.turnLeft(120.0);
+            zofka.move(size);
         }
     }
 
-    public void drawSquare(double a) {
+    private void drawIsocelesTriangle(double base) {
+        zofka.turnLeft(90.0);
+        zofka.move(base);
+
+        zofka.turnLeft(105);
+        zofka.move(base * 2);
+
+        zofka.turnLeft(150);
+        zofka.move(base * 2);
+    }
+
+    private void drawRightIsocelesTriangle(double size) {
+        zofka.move(size);
+        zofka.turnLeft(90.0);
+
+        zofka.move(size);
+        zofka.turnLeft(135.0);
+
+        zofka.move(Math.sqrt(2 * Math.pow(size, 2)));
+        zofka.turnLeft(135.0);
+    }
+
+    private void drawSquare(double a) {
         for (int i = 0; i < 3; i++) {
             zofka.move(a);
             zofka.turnLeft(90.0);
@@ -37,7 +70,7 @@ public class HlavniProgram {
         zofka.move(a);
     }
 
-    public void drawRectangle(double a, double b) {
+    private void drawRectangle(double a, double b) {
         zofka.move(a);
         zofka.turnLeft(90.0);
         zofka.move(b);
@@ -47,7 +80,7 @@ public class HlavniProgram {
         zofka.move(b);
     }
 
-    public void drawPolygon(int steps, double stepLength, double circleAngle) {
+    private void drawPolygon(int steps, double stepLength, double circleAngle) {
         for (int i = 0; i < steps; i++) {
             zofka.turnLeft(circleAngle);
             zofka.move(stepLength);
