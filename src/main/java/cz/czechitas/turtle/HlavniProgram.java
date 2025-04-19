47920b48b37fc7778db24f5b50fd5b8953751cc0
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-04-19 12:57:59 +0200

    implement methods for drawing basic parametric shapes

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package cz.czechitas.turtle;
 
 import dev.czechitas.java1.turtle.engine.Turtle;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
@@ -10,7 +12,45 @@ public class HlavniProgram {
     }
 
     public void start() {
+        zofka.setPenColor(Color.black);
+        zofka.setPenWidth(4);
+
         //TODO Tady bude kód pro kreslení želví grafiky.
+//        drawEquilateralTriangle(100.0);
+//        drawSquare(100.0);
+//        drawRectangle(50.0, 100.0);
+        drawPolygon(23, 12, 16);
     }
 
+    public void drawEquilateralTriangle(double size) {
+        for (int i = 0; i < 3; i++) {
+            zofka.move(size);
+            zofka.turnLeft(120.0);
+        }
+    }
+
+    public void drawSquare(double a) {
+        for (int i = 0; i < 3; i++) {
+            zofka.move(a);
+            zofka.turnLeft(90.0);
+        }
+        zofka.move(a);
+    }
+
+    public void drawRectangle(double a, double b) {
+        zofka.move(a);
+        zofka.turnLeft(90.0);
+        zofka.move(b);
+        zofka.turnLeft(90.0);
+        zofka.move(a);
+        zofka.turnLeft(90.0);
+        zofka.move(b);
+    }
+
+    public void drawPolygon(int steps, double stepLength, double circleAngle) {
+        for (int i = 0; i < steps; i++) {
+            zofka.turnLeft(circleAngle);
+            zofka.move(stepLength);
+        }
+    }
 }
